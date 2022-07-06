f71d2ed2c37be1fb177d45a4e44f7c9333b2eb43
juanpalo25 <juanpalo25@hotmail.com>
2022-07-06 17:31:32 -0300

    se agrega una nueva APP y se modifica el modelo Blog

--- lectores/models.py
+++ lectores/models.py
@@ -4,4 +4,6 @@ class Blog(models.Model):
     titulo = models.CharField(max_length=35)
     caracteres = models.IntegerField()
     fecha_creacion = models.DateField(null=True)
+    genero = models.CharField(max_length=20)
+    autor = models.CharField(max_length=15)
 
