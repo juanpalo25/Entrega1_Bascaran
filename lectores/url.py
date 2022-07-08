88a830a8f94461d9814406d70eea4d175df91664
juanpalo25 <juanpalo25@hotmail.com>
2022-07-08 18:12:48 -0300

    Se agrega el CRUD al proyecto

--- lectores/url.py
+++ lectores/url.py
@@ -1,9 +1,13 @@
-from .views import home, crear_blog, vista_base, listado_blogs
+from .views import home, crear_blog, vista_base, listado_blogs, editar_blog, eliminar_blog
 from django.urls import path
+
 
 urlpatterns = [
     path('', home, name= 'home'),
     path('crear_blog/', crear_blog, name='crear_blog'),
     path('base/', vista_base),
     path('listado_blogs/', listado_blogs, name='listado_blogs'),
+    path('editar_blog/<int:id>', editar_blog, name='editar_blog'),
+    path('eliminar_blog/<int:id>', eliminar_blog, name='eliminar_blog'),
 ]
+
--- lectores/views.py
+++ lectores/views.py
@@ -49,3 +49,29 @@ def listado_blogs(request):
 
 def vista_base(request):
     return render(request, 'base.html')
+
+def editar_blog(request, id):
+    blog = Blog.objects.get(id = id)
+
+    if request.method == 'POST':
+        form = FormBlog(request.POST)
+        if form.is_valid():
+            blog = Blog.objects.get(id = id)    
+            blog.titulo = form.cleaned_data.get('titulo')
+            blog.caracteres = form.cleaned_data.get('caracteres')
+            blog.fecha_creacion = form.cleaned_data.get('fecha_creacion')
+            blog.genero = form.cleaned_data.get('genero')
+            blog.autor = form.cleaned_data.get('autor')
+            blog.save()
+            return redirect('listado_blogs')
+        else: 
+            return render(request, 'editar_blog.html',{'form': form, 'blog':blog})
+    form_blog = FormBlog(initial={'titulo': blog.titulo, 'caracteres': blog.caracteres, 'fecha_creacion':blog.fecha_creacion, 'genero': blog.genero, 'autor': blog.autor})
+    return render(request, 'editar_blog.html', {'form': form_blog, 'blog': blog} )
+
+
+def eliminar_blog(request, id):
+    blog =Blog.objects.get(id = id)
+    blog.delete()
+     
+    return redirect('listado_blogs')
